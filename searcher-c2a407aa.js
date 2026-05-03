dbe4aa13e39c0032fc99965f7ec74982721a1643
zeenix <zeenix@users.noreply.github.com>
2026-05-03 16:44:45 +0000

    Deploying to gh-pages from @ z-galaxy/zbus@2271f593d64d1d40eb0b78a53ecb335072f0ff0c 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7e58e975.js',
+                path_to_root + 'searchindex-1a8ad000.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
